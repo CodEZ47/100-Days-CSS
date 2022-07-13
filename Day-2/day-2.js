9f34b28b518dc15fd9885ccc028f1f105c0af092
CodEZ47 <88854432+CodEZ47@users.noreply.github.com>
2022-07-13 22:36:10 +0300

    Update day-2.js

--- Day-2/day-2.js
+++ Day-2/day-2.js
@@ -1,7 +1,12 @@
+let bool = 0;
 function toggle(){
-	const lines = document.getElementsByClassName('no-animation');
-	for(let elem in lines){
-		elem.classList.remove('no-animation');
+	if(bool == 0){
+		const lines = document.getElementsByClassName('lines');
+		console.log(lines);
+		for(let i = 0; i < 3; i++){
+			lines[i].classList.remove('no-animation');
+		};
+		bool = 1;
 	};
 	const menu = document.getElementById('menu');
 	menu.classList.toggle('clicked');
